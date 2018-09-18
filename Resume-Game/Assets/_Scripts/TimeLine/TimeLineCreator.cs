c36e84b46915a778bc81ba890cb73f051017fc56
RameshBettge <bettge.r@gmx.de>
2018-09-18 11:32:08 +0200

    Created. Structs for managing time-values. Started to create the Bio's timeline

---
+++ Resume-Game/Assets/_Scripts/TimeLine/TimeLineCreator.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimeLineCreator : MonoBehaviour
+{
+    public Phase scope;
+    public Phase[] phases;
+    Text t;
+
+    Dictionary<int, string> monthLookup = new Dictionary<int, string>();
+    
+
+    void Start()
+    {
+        t = GetComponentInChildren<Text>();
+        t.text = scope.start.GetString();
+    }
+
+    void MapScope(Phase p)
+    {
+        int start = p.start.Position - scope.start.Position;
+        int end = p.end.Position - scope.start.Position;
+    }
+
+    void Update()
+    {
+
+    }
+}
+
---
+++ Resume-Game/Assets/_Scripts/TimeLine/TimeStructs.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct Date
+{
+    public int day;
+    public Month month;
+    public int year;
+
+    public int Position
+    {
+        get { return year * 12 + (int)month; }
+    }
+
+
+    public string GetString()
+    {
+        string o = "";
+        string sDay = day.ToString();
+
+        if (day > 0)
+        {
+            if(day == 11)
+            {
+                o += "11th";
+            }
+            else if(sDay[sDay.Length -1] == '1')
+            {
+                o += sDay + "st";
+            }
+            else if (sDay[sDay.Length - 1] == '2')
+            {
+                o += sDay + "nd";
+            }
+            else if(sDay[sDay.Length - 1] == '3')
+            {
+                o += sDay + "rd";
+            }
+            else
+            {
+                o += sDay + "th";
+            }
+
+            o += " of ";
+        }
+
+        o += month.ToString() + " ";
+        o += year;
+
+        if (year.ToString().Length < 4 || month.ToString().Length < 1)
+        {
+            Debug.LogWarning("This date is not set properly! The standard format is: 'dd.mm.yyyy' OR 'mm.yyyy' . This date is set to: " + o);
+        }
+
+        return o;
+    }
+}
+
+public enum Month
+{
+    January = 1, February, March, April, May, June, July, August, September, October, November, December
+}
+
+[System.Serializable]
+public struct Phase
+{
+    public string title;
+
+    public Date start;
+    public bool inProgress;
+    public Date end;
+}
+
