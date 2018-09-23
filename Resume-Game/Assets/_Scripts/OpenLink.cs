9dbedd875991052cc9e05670b5e879d94eeb88b7
RameshBettge <bettge.r@gmx.de>
2018-09-23 13:29:14 +0200

    CREATED: OpenLink.cs. CHANGED: Hire Button is now called 'Contact' and opens a page in the browser when clicked.

---
+++ Resume-Game/Assets/_Scripts/OpenLink.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour {
+
+    [SerializeField]
+    string url;
+
+	public void GoToURL()
+    {
+        Application.OpenURL(url);
+    }
+}
