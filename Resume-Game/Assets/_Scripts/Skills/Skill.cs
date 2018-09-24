52839de292b302575f99b85841b925e9e998dc44
RameshBettge <bettge.r@gmx.de>
2018-09-24 13:55:39 +0200

    ADDED: Skill bars are now instantiated automatically. Their buttons display the correlating info.

---
+++ Resume-Game/Assets/_Scripts/Skills/Skill.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    public string title;
+    public float expPercentage;
+    public float passionPercentage;
+
+
+    private void Awake()
+    {
+        expPercentage = CheckPercentage(expPercentage);
+        passionPercentage = CheckPercentage(passionPercentage);
+    }
+
+    float CheckPercentage(float p)
+    {
+        if (expPercentage > 1f || expPercentage < 0f)
+        {
+            Debug.LogWarning("Skill percentage was set to " + p + ". It has been clamped.");
+            return Mathf.Clamp01(p);
+        }
+        else
+        {
+            return p;
+        }
+    }
+}
--- Resume-Game/Assets/_Scripts/Skills/SkillManager.cs
+++ Resume-Game/Assets/_Scripts/Skills/SkillManager.cs
@@ -1,16 +1,184 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SkillManager : MonoBehaviour {
+public class SkillManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject skillTemplate;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    Transform hardSkillPanel;
+    [SerializeField]
+    Transform softSkillPanel;
+
+    public Skill[] hardSkills;
+    public Skill[] softSkills;
+
+    Transform hardParent;
+    Transform softParent;
+
+    bool softActive = false;
+    GameObject currentSkillDisplay;
+
+    List<Button> hardButtons;
+    List<Button> softButtons;
+
+    void Start()
+    {
+        hardButtons = new List<Button>();
+        softButtons = new List<Button>();
+
+        hardParent = transform.GetChild(0);
+        softParent = transform.GetChild(1);
+
+        hardSkills = hardParent.GetComponentsInChildren<Skill>(true);
+        softSkills = softParent.GetComponentsInChildren<Skill>(true);
+
+        SetInfos(hardParent, hardSkills);
+        SetInfos(softParent, softSkills);
+
+        FillList(hardSkills, hardSkillPanel);
+        FillList(softSkills, softSkillPanel);
+
+        softSkillPanel.gameObject.SetActive(false);
+
+        SetButtons(hardButtons, true);
+    }
+
+    void SetInfos(Transform p, Skill[] skills)
+    {
+        for (int i = 0; i < p.childCount; i++)
+        {
+            GameObject child = p.GetChild(i).gameObject;
+            child.SetActive(false);
+
+            SetTitle(child.transform, skills[i]);
+            skills[i].gameObject.name = skills[i].title;
+        }
+    }
+
+    void SetTitle(Transform p, Skill s)
+    {
+        for (int i = 0; i < p.childCount; i++)
+        {
+            Transform child = p.GetChild(0);
+            if (child.CompareTag("Title"))
+            {
+                child.GetComponent<Text>().text = s.title;
+            }
+            else
+            {
+                SetTitle(child, s);
+            }
+        }
+    }
+
+    void FillList(Skill[] skills, Transform skillPanel)
+    {
+        Transform skillList = null;
+
+        for (int i = 0; i < skillPanel.childCount; i++)
+        {
+            if (skillPanel.GetChild(i).CompareTag("List"))
+            {
+                skillList = skillPanel.GetChild(i);
+            }
+        }
+
+        for (int i = 0; i < skills.Length; i++)
+        {
+            Transform t = Instantiate(skillTemplate, skillList).transform;
+
+            ApplySkillValues(t, skills[i], 0);
+
+            Button b = t.GetComponentInChildren<Button>();
+
+            //'idx' has to be created because otherwise 'i' will always be equal to skills.length.
+            int idx = i;
+            b.onClick.AddListener(delegate { DisplaySkill(idx); });
+
+
+            if (skillPanel == hardSkillPanel)
+            {
+                hardButtons.Add(b);
+            }
+            else
+            {
+                softButtons.Add(b);
+            }
+        }
+    }
+
+    void DisplaySkill(int idx)
+    {
+        if (currentSkillDisplay != null)
+        {
+            currentSkillDisplay.SetActive(false);
+        }
+
+        if (softActive)
+        {
+            print("idx " + idx + " softSkills.Length " + softSkills.Length);
+            currentSkillDisplay = softSkills[idx].gameObject;
+        }
+        else
+        {
+            print("idx " + idx + " hardSkills.Length " + hardSkills.Length);
+            currentSkillDisplay = hardSkills[idx].gameObject;
+        }
+
+        currentSkillDisplay.SetActive(true);
+    }
+
+    //Optimization: Function doesn't return if all three components have been found.
+    void ApplySkillValues(Transform parent, Skill skill, int found)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            if (child.CompareTag("ExpFill"))
+            {
+                child.GetComponent<Image>().fillAmount = skill.expPercentage;
+                found++;
+            }
+            else if (child.CompareTag("PassionFill"))
+            {
+                child.GetComponent<Image>().fillAmount = skill.passionPercentage;
+                found++;
+            }
+            else if (child.CompareTag("Title"))
+            {
+                parent.name = skill.title;
+                child.GetComponent<Text>().text = skill.title;
+                found++;
+            }
+            else
+            {
+                ApplySkillValues(child, skill, found);
+            }
+        }
+    }
+
+
+    public void DisableAllButtons()
+    {
+        SetButtons(hardButtons, false);
+        SetButtons(softButtons, false);
+    }
+
+    void SetButtons(List<Button> buttons, bool b)
+    {
+        foreach (Button button in buttons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    public void SwitchSkillCategory()
+    {
+
+    }
+
 }
